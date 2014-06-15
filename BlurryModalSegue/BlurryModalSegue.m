4e1269290498a689b9d0c2ffee68fb96f122f961
noark9 <noark9@gmail.com>
2014-06-15 23:09:58 +0800

    using blur effect in fxblurview

--- BlurryModalSegue/BlurryModalSegue.m
+++ BlurryModalSegue/BlurryModalSegue.m
@@ -10,6 +10,7 @@
 #import <QuartzCore/QuartzCore.h>
 #import <UIImage+BlurredFrame/UIImage+ImageEffects.h>
 #import <MZAppearance/MZAppearance.h>
+#import <FXBlurView.h>
 
 static UIImageOrientation ImageOrientationFromInterfaceOrientation(UIInterfaceOrientation orientation) {
     switch (orientation)
@@ -38,16 +39,16 @@ static UIImageOrientation ImageOrientationFromInterfaceOrientation(UIInterfaceOr
 - (id)initWithIdentifier:(NSString *)identifier source:(UIViewController *)source destination:(UIViewController *)destination
 {
     self = [super initWithIdentifier:identifier source:source destination:destination];
-    
+
     if (self)
     {
         // Some sane defaults
         self.backingImageBlurRadius = @(20);
         self.backingImageSaturationDeltaFactor = @(.45f);
-        
+
         [[[self class] appearance] applyInvocationTo:self];
     }
-    
+
     return self;
 }
 
@@ -57,15 +58,15 @@ static UIImageOrientation ImageOrientationFromInterfaceOrientation(UIInterfaceOr
     UIViewController* destination = (UIViewController*)self.destinationViewController;
 
     CGRect windowBounds = source.view.window.bounds;
-    
+
     // Normalize based on the orientation
     CGRect nomalizedWindowBounds = [source.view convertRect:windowBounds fromView:nil];
-    
+
     UIGraphicsBeginImageContextWithOptions(windowBounds.size, YES, 0.0);
 
     [source.view.window drawViewHierarchyInRect:windowBounds afterScreenUpdates:NO];
     UIImage *snapshot = UIGraphicsGetImageFromCurrentImageContext();
-    
+
     UIGraphicsEndImageContext();
 
     if (self.processBackgroundImage)
@@ -74,16 +75,15 @@ static UIImageOrientation ImageOrientationFromInterfaceOrientation(UIInterfaceOr
     }
     else
     {
-        snapshot = [snapshot applyBlurWithRadius:self.backingImageBlurRadius.doubleValue
-                                       tintColor:self.backingImageTintColor
-                           saturationDeltaFactor:self.backingImageSaturationDeltaFactor.doubleValue
-                                       maskImage:nil];
+        snapshot = [snapshot blurredImageWithRadius:self.backingImageBlurRadius.doubleValue
+                                         iterations:self.backingImageSaturationDeltaFactor.doubleValue
+                                          tintColor:self.backingImageTintColor];
     }
-    
+
     snapshot = [UIImage imageWithCGImage:snapshot.CGImage scale:1.0 orientation:ImageOrientationFromInterfaceOrientation([UIApplication sharedApplication].statusBarOrientation)];
-    
+
     destination.view.clipsToBounds = YES;
-    
+
     UIImageView* backgroundImageView = [[UIImageView alloc] initWithImage:snapshot];
 
     CGRect frame;
@@ -99,12 +99,12 @@ static UIImageOrientation ImageOrientationFromInterfaceOrientation(UIInterfaceOr
             break;
     }
     backgroundImageView.frame = frame;
-    
+
     [destination.view addSubview:backgroundImageView];
     [destination.view sendSubviewToBack:backgroundImageView];
-    
+
     [self.sourceViewController presentModalViewController:self.destinationViewController animated:YES];
-    
+
     [destination.transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
         [UIView animateWithDuration:[context transitionDuration] animations:^{
             backgroundImageView.frame = CGRectMake(0, 0, nomalizedWindowBounds.size.width, nomalizedWindowBounds.size.height);
